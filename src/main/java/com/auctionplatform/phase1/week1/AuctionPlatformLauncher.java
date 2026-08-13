b148f296f7f811f650d139a6b4bdd5e78cdab386
kibagendi20 <wesoon98@gmail.com>
2026-08-13 23:59:47 +0300

    initial setup

---
+++ src/main/java/com/auctionplatform/phase1/week1/AuctionPlatformLauncher.java
@@ -0,0 +1,9 @@
+package com.auctionplatform.phase1.week1;
+
+public class AuctionPlatformLauncher {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+
+
+}
